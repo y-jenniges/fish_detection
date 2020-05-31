7019cbd6fb31807d8565214a3e3edcb201620937
Onny <onny.je@googlemail.com>
2020-05-31 08:19:03 +0200

    added loading of testGen, model and history

--- neuralNet/test_lowRes.py
+++ neuralNet/test_lowRes.py
@@ -3,6 +3,9 @@ import pickle
 import os
 from datetime import datetime
 import keras
+import HelperFunctions as helpers
+
+test_path = "../data/tests/1/"
 
 # load annotation files
 path = "../data/labels/training_labels_animals.json"
@@ -19,18 +22,22 @@ path = "../data/labels/training_labels_no_animals.json"
 with open(os.path.join(label_root, path), 'r') as f:
     train_labels_no_animals = json.load(f)
 
-# load testGenL
-testGenL = None
 
 # load testGenL
-with open("serialized_testGen.pickle",'rb') as file:
+with open(os.path.join(test_path,"serialized_testGen.pickle"),'rb') as file:
     raw_data = file.read()
-test = pickle.loads(raw_data)
+testGen = pickle.loads(raw_data)
 
 
 # load model
 # todo look for file name (depends on timestamp)
-modelL = load_model()
+modelL = keras.models.load_model("../data/tests/1/model-L-20200531-015257")
+
+# load training history
+with open(os.path.join(test_path,"trainHistory-20200531-015257"),'rb') as file:
+    raw_data = file.read()
+history = pickle.loads(raw_data)
+
 
 # Use this to visualize results
 # It shows the input image (background), the predicted heatmap (white "fog" in foreground)
@@ -45,18 +52,20 @@ modelL = load_model()
 output_json = []
 gt = []
 yHats = []
-#for i in range(len(testGenL)):
+#for i in range(len(testGen)):
 for i in range(2):
     temp = {}
-    testBatch = testGenL[i]
+    testBatch = testGen[i]
     
     temp['filename'] = testBatch[0][i]
     temp['batch'] = i
-    temp['groundtruth'] = testGenL.get_ground_truth(i)
+    temp['groundtruth'] = testGen.get_ground_truth(i)
     temp['prediction'] = modelL.predict (testBatch[0])
+    
+   # helpers.showImageWithHeatmap(image, hm=None, gt=None, group=1, bodyPart="front", filename=None)
     
     output_json.append(temp)
     
 # save output
-with open(f"../data/output/predictions-{str(datetime.now())}.json", 'w') as outfile:
-    json.dump(output_json, outfile)
+# with open(f"../data/output/predictions-{str(datetime.now())}.json", 'w') as outfile:
+#     json.dump(output_json, outfile)
