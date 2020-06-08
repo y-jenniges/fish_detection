1df5484bd4685f67ee0c0c892cdb41e53f2c3835
Onny <onny.je@googlemail.com>
2020-06-08 11:20:12 +0200

    added script for calculating high resolution heatmap (backward way/upsampling)

---
+++ neuralNet/train_highRes.py
@@ -0,0 +1,180 @@
+import DataGenerator as dg
+import HelperFunctions as helpers
+import Globals
+import keras
+from keras import layers
+import time
+import json
+import os
+import pickle
+
+"""group: 
+    0 - nothing, 
+    1 - fish, 
+    2 - crustacea, 
+    3- chaetognatha, 
+    4 - unidentified_object, 
+    5 - jellyfish
+bodyPart: 
+    'front'
+    'back'
+    'both'
+"""
+# output directory
+#timestamp = time.strftime("%Y%m%d-%H%M%S")
+out_path = f"../data/output/11/"
+
+# load annotation files
+#label_root = "../data/maritime_dataset/labels/"
+label_root = "../data/maritime_dataset_25/labels/"
+
+label_path = "training_labels_animals.json"
+with open(os.path.join(label_root, label_path) , 'r') as f:
+    train_labels_animals = json.load(f)
+    
+label_path = "test_labels.json"
+with open(os.path.join(label_root, label_path), 'r') as f:
+    test_labels = json.load(f)
+    
+label_path = "training_labels_no_animals.json"
+with open(os.path.join(label_root, label_path), 'r') as f:
+    train_labels_no_animals = json.load(f)
+
+
+# image path
+#data_root = "../data/maritime_dataset/"
+data_root = "../data/maritime_dataset_25/"
+imageShape = helpers.shapeOfFilename(os.path.join(data_root,"training_data_animals/0.jpg"))
+print(f"Image format {imageShape}.")
+
+trainGenH = dg.DataGenerator (dataset=train_labels_animals,
+                              #hm_folder_path="../data/heatmaps_lowRes/training_animals/",
+                              no_animal_dataset=train_labels_no_animals,
+                              no_animal_ratio=Globals.no_animal_ratio,
+                              prepareEntry=dg.prepareEntryHighResHeatmap,
+                              batch_size=Globals.batch_size)
+
+testGenH = dg.DataGenerator (dataset=test_labels, 
+                              #hm_folder_path="../data/heatmaps_lowRes/test/" ,
+                              prepareEntry=dg.prepareEntryHighResHeatmap,
+                              batch_size=Globals.batch_size)
+
+print("DataGenerators initialized")
+
+
+dg.showEntryOfGenerator (trainGenH, 0, showHeatmaps=False)
+dg.showEntryOfGenerator (testGenH, 0, False)
+
+
+# serialize data generators
+serialized_trainGen = pickle.dumps(trainGenH)
+serialized_testGen = pickle.dumps(testGenH)
+
+filename_train = f'{out_path}serialized_trainGen-H.pickle'
+filename_test = f'{out_path}serialized_testGen-H.pickle'
+
+# save data generators
+with open(filename_train,'wb') as file:
+    file.write(serialized_trainGen)
+with open(filename_test,'wb') as file:
+    file.write(serialized_testGen)
+
+print("DataGenerators serialized")
+
+
+# load model for low resolution
+modelL = keras.models.load_model(f"{out_path}model-L")
+
+# Now construct the low-res net and store it into the variable model
+# Loading of MobileNet.V2 will give a warning "`input_shape` is undefined or non-square, or `rows` is not in [96, 128, 160, 192, 224]. Weights for input shape (224, 224) will be loaded as the default."
+# That's correct.
+# Proceed as described in abv-uebung-6.pdf and discuss the architecture.
+# Inspect the architecture with model.summary() to make sure, that the number of trainable weights
+# is compatible to the amount of data we have.
+#
+# For reference you can access the MobileNet.V2 source code at
+# https://github.com/keras-team/keras-applications/blob/master/keras_applications/mobilenet_v2.py
+
+def ourBlock (x, basename, channels=8):
+#def ourBlock(x, basename, channels=Globals.channels):
+    """Our own block of computation layers used several times in the network. It is similar to
+    the block used in MobileNet.V2 but simplified. x is the layer to attach the block to,
+    basename the name of this block, which will be extended by layer names. channel is the
+    number of channels in the output (internally a multiple of that). The final layer is returned."""
+    # First reduce to the defined no of channels to avoid having too many weights
+    x = layers.Conv2D (channels, 1, padding='same', name = basename+"_bottleneck_conv")(x)
+    x = layers.BatchNormalization(axis=-1, epsilon=1e-3, momentum=0.999, name=basename+'_bottleneck_BN')(x)
+    x = layers.ReLU(6., name=basename+'_bottleneck_relu')(x)
+
+    x = layers.Conv2D (4*channels, 1, padding='same', name = basename+"_conv")(x)
+    x = layers.BatchNormalization(axis=-1, epsilon=1e-3, momentum=0.999, name=basename+'_BN')(x)
+    x = layers.ReLU(6., name=basename+'_relu')(x)
+
+    x = layers.DepthwiseConv2D(kernel_size=3, activation=None, use_bias=False, padding='same', name=basename+'-DW')(x)
+    x = layers.BatchNormalization(axis=-1, epsilon=1e-3, momentum=0.999, name=basename+'_DW_BN')(x)
+    x = layers.ReLU(6., name=basename+'_DW_relu')(x)
+    
+    x = layers.Conv2D (channels, 1, padding='same', name = basename+"_project")(x)
+    x = layers.BatchNormalization(axis=-1, epsilon=1e-3, momentum=0.999, name=basename+'_project_BN')(x)
+    
+    return x
+
+x = modelL.get_layer("block_17_project_BN").output # low-res model before last conv-sigmoid layer
+x = layers.UpSampling2D(interpolation='bilinear', name='block_19_upto16')(x)
+# Concatenate with the layer before the expansion that is followed by conv and downsample
+x = layers.Concatenate(name="block_19_concat")([x,modelL.get_layer("block_12_add").output])
+x = ourBlock (x, 'block_19')
+
+x = layers.UpSampling2D(interpolation='bilinear', name='block_20_upto8')(x)
+x = layers.Concatenate(name="block_20_concat")([x,modelL.get_layer("block_5_add").output])
+x = ourBlock (x, 'block_20')
+
+x = layers.UpSampling2D(interpolation='bilinear', name='block_21_upto4')(x)
+x = layers.Concatenate(name="block_21_concat")([x,modelL.get_layer("block_2_add").output])
+x = ourBlock (x, 'block_21')
+
+x = layers.UpSampling2D(interpolation='bilinear', name='block_22_upto2')(x)
+x = layers.Concatenate(name="block_22_concat")([x,modelL.get_layer("expanded_conv_project_BN").output])
+x = ourBlock (x, 'block_22')
+
+x = layers.UpSampling2D(interpolation='bilinear', name='block_23_upto1')(x)
+#x = layers.Concatenate(name="block_23_concat")([x, input])
+#x = layers.Conv2D (8, 3, padding='same', name = "block_23_conv")(x)
+#x = layers.BatchNormalization(axis=-1, epsilon=1e-3, momentum=0.999, name='block_23_BN')(x)
+#x = layers.ReLU(6., name='block_23_relu')(x)
+x = ourBlock (x, 'block_23', channels=4)
+
+x = layers.Conv2D (1, 1, padding='same', activation=Globals.activation_outLayer, name = "block_23_conv_output")(x)
+
+
+input_tensor = keras.layers.Input(shape=imageShape)
+
+modelH = keras.Model(inputs=input_tensor, outputs=x)
+modelH.compile(loss=Globals.loss, optimizer=Globals.optimizer, metrics=Globals.metrics)
+modelH.summary()
+
+
+
+# take time of training process
+start  = time.time()
+
+# train low-res-net
+#model.load_weights ("strawberry-L.h5"), #load a previous checkpoint
+#for ctr in range(10):
+history = modelH.fit_generator(generator=trainGenH, epochs=Globals.epochs, validation_data=testGenH)
+
+# print the time used for training
+print(f"Training took {time.time() - start}")
+
+
+# save model, weights and history
+modelH.save(f"{out_path}model-H")
+modelH.save_weights(f"{out_path}weights-H.h5") # saves weights (e.g. a checkpoint) locally
+# save the history(todo: is it already contained in modelL.save? and also weights?)
+# history.history is a dict
+with open(f"{out_path}trainHistory-H.pickle", 'wb') as file:
+    pickle.dump(history.history, file)
+    #modelL.save_weights(f"fish-L-{ctr}.h5") # saves weights (e.g. a checkpoint) locally
+  
+    #files.download('fish-L.h5') # download weights from e.g. google-colab to local machine
+    
