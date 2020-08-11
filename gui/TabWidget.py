7508507994181404139baac97caa5cc056f0a99b
Onny <onny.je@googlemail.com>
2020-08-12 00:10:24 +0200

    implemented interactions for the settings page and did some renaming in the ui file and the py file

---
+++ gui/TabWidget.py
@@ -0,0 +1,38 @@
+# implementation of TabWidget and TabBar from https://www.manongdao.com/q-367474.html (last access: 11.08.2020)
+# allows the tabs to be placed on the left side with horizontal text (per default, Qt only supports vertical text here)
+from PyQt5 import QtCore, QtWidgets
+
+class TabWidget(QtWidgets.QTabWidget):
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QTabWidget.__init__(self, *args, **kwargs)
+        self.setTabBar(TabBar(self))
+        self.setTabPosition(QtWidgets.QTabWidget.West)
+    
+        
+class TabBar(QtWidgets.QTabBar):
+    def tabSizeHint(self, index):
+        s = QtWidgets.QTabBar.tabSizeHint(self, index)
+        s.transpose()
+        return s
+
+    def paintEvent(self, event):
+        painter = QtWidgets.QStylePainter(self)
+        opt = QtWidgets.QStyleOptionTab()
+
+        for i in range(self.count()):
+            self.initStyleOption(opt, i)
+            painter.drawControl(QtWidgets.QStyle.CE_TabBarTabShape, opt)
+            painter.save()
+
+            s = opt.rect.size()
+            s.transpose()
+            r = QtCore.QRect(QtCore.QPoint(), s)
+            r.moveCenter(opt.rect.center())
+            opt.rect = r
+
+            c = self.tabRect(i).center()
+            painter.translate(c)
+            painter.rotate(90)
+            painter.translate(-c)
+            painter.drawControl(QtWidgets.QStyle.CE_TabBarTabLabel, opt)
+            painter.restore()
