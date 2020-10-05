d73860223d42c0949dd21dc7a31824c2e150b165
Onny <onny.je@googlemail.com>
2020-10-05 23:03:35 +0200

    adapting data generator so one has multiple options to run (e.g. heatmap only, heatmaps and vectors, ...), adding file containing mobilenet, training none of its layers, run 200, for EVALUATION

--- neuralNet/DataGenerator.py
+++ neuralNet/DataGenerator.py
@@ -1,21 +1,24 @@
 # Tools to load the training / test set on the fly, so the whole dataset
 # doesn't need to be kept in memory.
-
-import numpy as np
-import keras
-import HelperFunctions as helpers
-import Globals
-import random
-import HeatmapClass
 import json
-import math
-import matplotlib.pyplot as plt
 import os
+import numpy as np
+import random
+import keras
+import HeatmapClass
+import HelperFunctions as helpers
+
+
+OPTION = "fish_heads"
+# "all_animals"
+# "body_segmentation"
+# "vector_fields" 
 
 def dummyPrepareEntry (entry, hm_folder):
     """Dummy function to prepare an entry of the dataset. It takes one entry
      and converts it to a input, ground-truth output pair that is given
      to keras. At the moment the image is loaded and the output is just empty."""
+    print("dummy prepare entry")
     return (helpers.loadImage(entry['filename']), [])
     
 def generateAllHeatmaps(entry, res='low'):
@@ -41,14 +44,9 @@ def generateAllHeatmaps(entry, res='low'):
     hm_5_tail = HeatmapClass.Heatmap(entry, resolution=res, group=5, bodyPart="back")
     hm_5_body = HeatmapClass.Heatmap(entry, resolution=res, group=5, bodyPart="connection")
                   
-
-
-    
-    #hm.showImageWithHeatmap()
-    #hm = helpers.downsample(hm.hm)
+    # downsampling
     if res=="low": f=32
     else: f=2
-    #hm_0.downsample()
     hm_1_head.downsample(f)
     hm_1_tail.downsample(f)
     hm_1_body.downsample(f)
@@ -76,14 +74,8 @@ def generateAllHeatmaps(entry, res='low'):
     
     # assemble connection head-tail heatmap 
     hm_body = helpers.sumHeatmaps([hm_1_body.hm, hm_2_body.hm, hm_3_body.hm, hm_4_body.hm, hm_5_body.hm])
-    #hm_body = np.clip (hm_body, 0, 1, out=hm_body)
     
-
-    # image = helpers.loadImage(entry['filename'])
-    # helpers.downsample(image)
-   # hm_y, hm_x = image.shape[0], image.shape[1]
-    
-   # nothing heatmap
+    # nothing heatmap
     hm_y, hm_x = hm_1_head.hm.shape[0], hm_1_head.hm.shape[1]
     hm_0 = np.ones((hm_y, hm_x, 1), dtype=np.float32)
     hm_0 -= hm_1_head.hm + hm_1_tail.hm + \
@@ -93,10 +85,7 @@ def generateAllHeatmaps(entry, res='low'):
             hm_5_head.hm + hm_5_tail.hm + hm_body
     
     hm_0 = np.clip (hm_0, 0, 1, out=hm_0)
-    
 
-        
-    
     return [hm_0, 
             hm_1_head.hm, hm_1_tail.hm, 
             hm_2_head.hm, hm_2_tail.hm,
@@ -106,87 +95,102 @@ def generateAllHeatmaps(entry, res='low'):
             ], [head_vectors, tail_vectors]#, hm_heads, hm_tails]
             #hm_body
 
-def prepareEntryLowResHeatmap (entry, hm_folder=None):
-    """Get's an entry of the dataset (filename, annotation), load filename and
-    converts annotation into a low-res heatmap. Returning both as x, y pair.
-    to be passed to keras."""
+def generateFishHeadHeatmaps(entry, res="low"):
+    # generate heatmap
+    hm_1_head = HeatmapClass.Heatmap(entry, resolution=res, group=1, bodyPart="front")
+    
+    # perform downsampling
+    if res=="low": 
+        f=32
+    else: 
+        f=2
+    hm_1_head.downsample(f)
+    
+    return hm_1_head.hm
 
-    heatmaps=[]
-    
-    # use precalculated heatmaps, if their folder is specified
-    if hm_folder != None:
-        hm_file_path = hm_folder + entry['filename'].split("/")[-1].rsplit(".jpg",1)[0] + '.json'    
-        with open(hm_file_path, 'r') as f:
-            hm_json = json.load(f)
-            hm = np.array(hm_json['hm_1f'])
-            # todo adapt the precalculated heatmaps! (i.e. clip them to 0-1)
-            np.clip (hm, 0, 1, out=hm)
-    else:
-        #print("Calculating heatmap...")
-        #head_vectors, tail_vectors = helpers.get_head_tail_vectors(entry)
-        #heatmaps, hm_body = generateAllHeatmaps(entry, res="low")
-        heatmaps, vectors = generateAllHeatmaps(entry, res="low")
-        
-    
+
+def prepareEntryHeatmap (entry, res="low"):
+    """Get's an entry of the dataset (filename, annotation), load filename and
+    converts annotation into a heatmap in desired resolution 
+    (low:1/32, high:1/2). Returning both as x, y pair to be passed to keras."""
+
     # load image and make its range [-1,1] (suitable for mobilenet)
     image = helpers.loadImage(entry['filename'], 32)
     image = 2.*image/np.max(image) - 1
-    #image = helpers.downsample(image, factor=2)
     
-    #classification=[]
- 
-    # heatmaps = [hm_1_head.hm, hm_1_tail.hm, 
-    #             hm_2_head.hm, hm_2_tail.hm,
-    #             hm_3_head.hm, hm_3_tail.hm,
-    #             hm_4_head.hm, hm_4_tail.hm,
-    #             hm_5_head.hm, hm_5_tail.hm,
-    #             ]
-    #heatmaps = keras.backend.stack(heatmaps)
-    heatmaps = np.concatenate(heatmaps, axis=2)
-    vectors = np.concatenate(vectors, axis=2)
+    # generate y depending on option
+    if OPTION == "fish_heads":
+        heatmap = generateFishHeadHeatmaps(entry, res=res)
+        return np.asarray(image), np.asarray(heatmap)
     
-    #return np.asarray(image), np.asarray(heatmaps), np.asarray(hm_body)
-    return np.asarray(image), np.asarray(heatmaps), np.asarray(vectors)
-
-def prepareEntryHighResHeatmap (entry, hm_folder=None):
-    """Get's an entry of the dataset (filename, annotation), load filename and
-    converts annotation into a low-res heatmap. Returning both as x, y pair.
-    to be passed to keras."""
-    
-    # filename = entry['filename']
-    # annotation = entry['animals']
-    heatmaps=[]
-    
-    # heatmap = HeatmapClass.Heatmap(entry, resolution='high', group=1, bodyPart='front')
-        # use precalculated heatmaps, if their folder is specified
-    if hm_folder != None:
-        hm_file_path = hm_folder + entry['filename'].split("/")[-1].rsplit(".jpg",1)[0] + '.json'    
-        with open(hm_file_path, 'r') as f:
-            hm_json = json.load(f)
-            hm = np.array(hm_json['hm_1f'])
-            # todo adapt the precalculated heatmaps! (i.e. clip them to 0-1)
-            np.clip (hm, 0, 1, out=hm)
-    else:
-        #print("Calculating heatmap...")
-        #heatmaps, hm_body = generateAllHeatmaps(entry, res="high")
-        heatmaps, vectors = generateAllHeatmaps(entry, res="high")
+    elif OPTION == "all_animals":
+        heatmaps = generateAllHeatmaps(entry, res=res) # @todo adapt
+        return 
         
-    #head_vectors, tail_vectors = helpers.get_head_tail_vectors(entry)
+    elif OPTION == "body_segmentation":
+        heatmaps, hm_body = generateAllHeatmaps(entry, res=res)# @todo adapt
+         # concatenate heatmaps
+        heatmaps = np.concatenate(heatmaps, axis=2)
+        
+        return np.asarray(image), np.asarray(heatmaps), np.asarray(hm_body)
     
-    # load image and make its range [-1,1] (suitable for mobilenet)
-    image = helpers.loadImage(entry['filename'], 32)#/np.array(128,dtype=np.float32)-np.array(1,dtype=np.float32)
-    #image = helpers.loadImage(entry['filename'])/np.array(128,dtype=np.float32)-np.array(1,dtype=np.float32)
-    #image = 2.*(image - np.min(image))/np.ptp(image)-1
-    image = 2.*image/np.max(image) - 1
-    #image = helpers.downsample(image, factor=2)
+    elif OPTION == "vector_fields":
+        heatmaps, vectors = generateAllHeatmaps(entry, res=res)# @todo adapt
 
-    heatmaps = np.concatenate(heatmaps, axis=2)
-    vectors = np.concatenate(vectors, axis=2)
+        # concatenate heatmaps, vectors
+        heatmaps = np.concatenate(heatmaps, axis=2)
+        vectors = np.concatenate(vectors, axis=2)
+        
+        return np.asarray(image), np.asarray(heatmaps), np.asarray(vectors)
+    
+    else:
+        print("Error: unknown option in data generator in prepareEntry!")
+        return 
+  
+def prepareEntryLowResHeatmap(entry):
+    return prepareEntryHeatmap(entry, "low")
+
+def prepareEntryHighResHeatmap (entry):
+    return prepareEntryHeatmap(entry, "high")
+    
+#     """Get's an entry of the dataset (filename, annotation), load filename and
+#     converts annotation into a low-res heatmap. Returning both as x, y pair.
+#     to be passed to keras."""
+    
+#     # filename = entry['filename']
+#     # annotation = entry['animals']
+#     heatmaps=[]
+    
+#     # heatmap = HeatmapClass.Heatmap(entry, resolution='high', group=1, bodyPart='front')
+#         # use precalculated heatmaps, if their folder is specified
+#     if hm_folder != None:
+#         hm_file_path = hm_folder + entry['filename'].split("/")[-1].rsplit(".jpg",1)[0] + '.json'    
+#         with open(hm_file_path, 'r') as f:
+#             hm_json = json.load(f)
+#             hm = np.array(hm_json['hm_1f'])
+#             # todo adapt the precalculated heatmaps! (i.e. clip them to 0-1)
+#             np.clip (hm, 0, 1, out=hm)
+#     else:
+#         #print("Calculating heatmap...")
+#         #heatmaps, hm_body = generateAllHeatmaps(entry, res="high")
+#         heatmaps, vectors = generateAllHeatmaps(entry, res="high")
+        
+#     #head_vectors, tail_vectors = helpers.get_head_tail_vectors(entry)
+    
+#     # load image and make its range [-1,1] (suitable for mobilenet)
+#     image = helpers.loadImage(entry['filename'], 32)#/np.array(128,dtype=np.float32)-np.array(1,dtype=np.float32)
+#     #image = helpers.loadImage(entry['filename'])/np.array(128,dtype=np.float32)-np.array(1,dtype=np.float32)
+#     #image = 2.*(image - np.min(image))/np.ptp(image)-1
+#     image = 2.*image/np.max(image) - 1
+#     #image = helpers.downsample(image, factor=2)
+
+#     heatmaps = np.concatenate(heatmaps, axis=2)
+#     vectors = np.concatenate(vectors, axis=2)
   
     
     
-#    return np.asarray(image), np.asarray(heatmaps), np.asarray(hm_body)
-    return np.asarray(image), np.asarray(heatmaps), np.asarray(vectors)
+# #    return np.asarray(image), np.asarray(heatmaps), np.asarray(hm_body)
+#     return np.asarray(image), np.asarray(heatmaps), np.asarray(vectors)
 
 def showEntryOfGenerator(dataGen, i, showHeatmaps=False):
     """Fetches the first batch, prints dataformat statistics and 
@@ -210,12 +214,12 @@ def showEntryOfGenerator(dataGen, i, showHeatmaps=False):
     #print(y['classification'].shape)
    #print(f"len of y: {len(y)} and shape of y[0]: {y[0].shape}")
     
-    print(f"y['heatmap'] has shape {y['heatmap'].shape}, y ['vectors'] has shape {y['vectors'].shape}")
+    #print(f"y['heatmap'] has shape {y['heatmap'].shape}, y ['vectors'] has shape {y['vectors'].shape}")
     
     #print(f"y['heatmap'] has shape {y['heatmap'].shape}, y ['connection'] has shape {y['connection'].shape}")
     #print(f"classification is {y['classification']}")
     print(f"X has shape {X.shape}, type {X.dtype} and range [{np.min(X):.3f}..{np.max(X):.3f}]") 
-
+    print(f"Y has shape {y.shape}")
 
     # todo how to i know the resolution
     #if showHeatmaps:
@@ -270,37 +274,58 @@ class DataGenerator(keras.utils.Sequence):
         #return int((np.floor(len(self.dataset)) + np.floor(self.no_animal_ratio*len(self.no_animal_dataset)))/ self.batch_size)
       
     def __getitem__(self, index):
-        #print("DataGenerator: getitem")
-        'Generate one batch of data'
-        
-        no_animal_hm_path = os.path.join(self.hm_folder_path, "../training_no_animals/") if self.hm_folder_path != None else None
+        'Generate one batch of data'   
+        #no_animal_hm_path = os.path.join(self.hm_folder_path, "../training_no_animals/") if self.hm_folder_path != None else None
 
-        
         batch_animals = self.dataset[index*self.animal_size:(index+1)*self.animal_size] 
         batch_no_animals = self.no_animal_dataset[index*self.no_animal_size:(index+1)*self.no_animal_size]
              
-        batch_animals = [self.prepareEntry(e, self.hm_folder_path) for e in batch_animals]
-        batch_no_animals = [self.prepareEntry(e, no_animal_hm_path) for e in batch_no_animals]
+        batch_animals = [self.prepareEntry(e) for e in batch_animals]
+        batch_no_animals = [self.prepareEntry(e) for e in batch_no_animals]
             
         batch = batch_animals + batch_no_animals
         
         X = np.array([e[0] for e in batch])
-        heatmaps = np.array([e[1] for e in batch])
-        #hm_body = np.array([e[2] for e in batch])
-        vectors = np.array([e[2] for e in batch])
         
-        #classification = np.array([e[2] for e in batch])
-        #y = {'heatmap': np.array([e[1] for e in batch]), 'classification': np.array([e[2] for e in batch])}
+        # get y depending on option
+        if OPTION == "fish_heads":
+            heatmaps = np.array([e[1] for e in batch])
+            return X, heatmaps
         
-        #print(f"y[heatmap] shape {np.array(y['heatmap']).shape}")
+        elif OPTION == "all_animals":
+            heatmaps = np.array([e[1] for e in batch])
+            return X, heatmaps # @todo adapt
+            
+        elif OPTION == "body_segmentation":
+            heatmaps = np.array([e[1] for e in batch])
+            hm_body = np.array([e[2] for e in batch])
+            return X, {"heatmap": heatmaps, "connection": hm_body}# @todo adapt
+        
+        elif OPTION == "vector_fields":
+            heatmaps = np.array([e[1] for e in batch])
+            vectors = np.array([e[2] for e in batch])
+            return X, {"heatmap": heatmaps, "vectors": vectors}# @todo adapt
+        
+        else:
+            print("Error: unknown option in data generator, get_item!")
+            return 
+        # X = np.array([e[0] for e in batch])
+        # heatmaps = np.array([e[1] for e in batch])
+        # #hm_body = np.array([e[2] for e in batch])
+        # vectors = np.array([e[2] for e in batch])
+        
+        # #classification = np.array([e[2] for e in batch])
+        # #y = {'heatmap': np.array([e[1] for e in batch]), 'classification': np.array([e[2] for e in batch])}
+        
+        # #print(f"y[heatmap] shape {np.array(y['heatmap']).shape}")
 
-        #print(f"heatmaps shape {np.asarray(heatmaps).shape}")
-        #print(f"heatmaps[0] shape {np.asarray(heatmaps[0]).shape}")
+        # #print(f"heatmaps shape {np.asarray(heatmaps).shape}")
+        # #print(f"heatmaps[0] shape {np.asarray(heatmaps[0]).shape}")
 
-        return X, {"heatmap":heatmaps, "vectors": vectors}
-        #return X, {"heatmap":heatmaps, "connection":hm_body}#heatmaps#{"heatmap": heatmaps, "classification": classification}
-        #return X, [heatmaps, classification]
-        #return X, heatmaps
+        # return X, {"heatmap":heatmaps, "vectors": vectors}
+        # #return X, {"heatmap":heatmaps, "connection":hm_body}#heatmaps#{"heatmap": heatmaps, "classification": classification}
+        # #return X, [heatmaps, classification]
+        # #return X, heatmaps
         
     def get_ground_truth (self, index):
         #print("DataGenerator: get_ground_truth")
