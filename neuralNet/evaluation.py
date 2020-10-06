8475884e9550e498a0cc9bc32dceffbb238d350c
Onny <onny.je@googlemail.com>
2020-10-06 09:08:01 +0200

    preparing evaluation script for transfer learning models

--- neuralNet/evaluation.py
+++ neuralNet/evaluation.py
@@ -2,13 +2,14 @@ import json
 import os
 import numpy as np
 import tensorflow as tf
-
+import keras
 import HelperFunctions as helpers
 import HeatmapClass
 import DataGenerator as dg
 
+BATCH_SIZE = 4
 
-test_path = "../data/output/100/"
+test_path = "../data/output/200/"
 model_path = "model-L"
 
 # load annotation files
@@ -18,48 +19,50 @@ path = "test_labels.json"
 with open(os.path.join(label_root, path), 'r') as f:
     test_labels = json.load(f)
     
-# load model
-model = tf.keras.models.load_model(os.path.join(test_path,model_path))
+test_labels = helpers.filter_labels_for_animal_group(test_labels)
 
-
-# testGen = dg.DataGenerator (dataset=test_labels, 
-#                               #hm_folder_path="../data/heatmaps_lowRes/test/" ,
-#                               prepareEntry=dg.prepareEntryLowResHeatmap,
-#                               batch_size=4)
-
-# # evaluate on test data
-# print("Evaluate on test data")
-# results = model.evaluate_generator(testGen)
-# print("test loss, test acc:", results)
-
-# minimalistic CNN
-def generateY(entry):
-    # load image and make its range [-1,1] (suitable for mobilenet)
-    image = helpers.loadImage(entry['filename'], 32)
-    image = 2.*image/np.max(image) - 1
-
-    heatmaps=[]
-
-    # fish head only
-    hm = HeatmapClass.Heatmap(entry, resolution='low', group=1, bodyPart="front")
-    hm.downsample(32)
+#test_labels[:4]
     
-    heatmaps = hm.hm
-    return np.asarray(image), np.asarray(heatmaps)
+# load model
+model = keras.models.load_model(os.path.join(test_path,model_path))
 
-x_test = []
-y_test = []
+testGen = dg.DataGenerator (dataset=test_labels, 
+                            prepareEntry=dg.prepareEntryLowResHeatmap,
+                            batch_size=BATCH_SIZE)
 
-print("generating heatmaps...")
-heatmaps=[]
-for entry in test_labels:
-    img, hms = generateY(entry)
-    x_test.append(img)
-    heatmaps.append(hms)
-print("assingning x and y...")
-y_test = np.asarray(heatmaps)
-x_test = np.asarray(x_test)
-
+# evaluate on test data
 print("Evaluate on test data")
-results = model.evaluate(x_test, y_test, batch_size=4, verbose=1)
-print("test loss, test mae:", results)
+results = model.evaluate_generator(testGen, len(test_labels)//BATCH_SIZE)
+print("test loss, test acc:", results)
+
+# # minimalistic CNN
+# def generateY(entry):
+#     # load image and make its range [-1,1] (suitable for mobilenet)
+#     image = helpers.loadImage(entry['filename'], 32)
+#     image = 2.*image/np.max(image) - 1
+
+#     heatmaps=[]
+
+#     # fish head only
+#     hm = HeatmapClass.Heatmap(entry, resolution='low', group=1, bodyPart="front")
+#     hm.downsample(32)
+    
+#     heatmaps = hm.hm
+#     return np.asarray(image), np.asarray(heatmaps)
+
+# x_test = []
+# y_test = []
+
+# print("generating heatmaps...")
+# heatmaps=[]
+# for entry in test_labels:
+#     img, hms = generateY(entry)
+#     x_test.append(img)
+#     heatmaps.append(hms)
+# print("assingning x and y...")
+# y_test = np.asarray(heatmaps)
+# x_test = np.asarray(x_test)
+
+# print("Evaluate on test data")
+# results = model.evaluate(x_test, y_test, batch_size=4, verbose=1)
+# print("test loss, test mae:", results)
