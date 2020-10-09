96a85bf14f352d54008730700c5cc7d8f182c672
Onny <onny.je@googlemail.com>
2020-10-09 17:00:05 +0200

    removing class weights here, now they re in datagenerator

--- neuralNet/lowRes_vectors.py
+++ neuralNet/lowRes_vectors.py
@@ -46,11 +46,10 @@ label_root = "../data/maritime_dataset_25/labels/"
 test_labels, train_labels, train_labels_no_animals, val_labels, class_weights = helpers.loadAndSplitLabels(label_root)
 
 # disable classweights if desired
-# disable classweights if desired
 if not USE_CLASSWEIGHTS: 
     class_weights = {0: 1, 1:1, 2:1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1, 10:1}
-
-print(f"class weights {class_weights}")
+sample_weight = np.array(list(class_weights.values()))
+print(f"sample weights {sample_weight}")
 
 # sample image
 data_root = "../data/maritime_dataset_25/"
@@ -62,11 +61,13 @@ trainGenL = dg.DataGenerator (dataset=train_labels,
                               no_animal_dataset=train_labels_no_animals,
                               no_animal_ratio=0,
                               prepareEntry=dg.prepareEntryLowResHeatmap,
-                              batch_size=BATCH_SIZE)
+                              batch_size=BATCH_SIZE,
+                              weights=sample_weight)
 
 valGenL = dg.DataGenerator (dataset=val_labels, 
                             prepareEntry=dg.prepareEntryLowResHeatmap,
-                            batch_size=BATCH_SIZE)
+                            batch_size=BATCH_SIZE,
+                            weights=sample_weight)
 
 print("DataGenerators initialized")
 
@@ -150,7 +151,7 @@ start  = time.time()
 history_1 = modelL.fit_generator(generator=trainGenL, 
                                  epochs=EPOCHS_1, 
                                  validation_data=valGenL, 
-                                 class_weight=class_weights
+                                 #class_weight=class_weights
                                  )
 
 # activate all layers for training
@@ -165,7 +166,7 @@ modelL.compile(loss={"heatmap": "categorical_crossentropy", "vectors": "mse"},
 history_2 = modelL.fit_generator(generator=trainGenL, 
                                  epochs=EPOCHS_2, 
                                  validation_data=valGenL, 
-                                 class_weight=class_weights
+                                 #class_weight=class_weights
                                  )
 
 # print the time used for training
